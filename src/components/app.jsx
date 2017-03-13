c266ff3598dedcae9faf047c20e54735fc6dee63
Felipe Rodrigues <fr.oliveira94@gmail.com>
2017-03-13 14:25:39 -0300

    config files

---
+++ src/components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <main className="mdl-layout__content">
+          <div className="page-content">
+            <div className="mdl-grid">
+              <div className="mdl-cell mdl-cell--3-col"></div>
+              <div className="mdl-cell mdl-cell--6-col">
+                teste
+              </div>
+              <div className="mdl-cell mdl-cell--3-col"></div>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+};
+
+export default App;
